ba753ddd5c3760ff051d246053b58e6a776f42d7
Irina Rai <lumencoeli81@gmail.com>
2016-09-24 17:51:26 +0500

    цветные квадратики

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,32 @@
 import parseCSV from './utilites/parse-csv'
 
+function squaresForEpisodes(episodes) {
+	let squares = ''
+	let i = 0;
+	while (i <  Number(episodes)) {
+	  squares += '<div class="cell dip"></div> '
+	  i++
+	}
+	return squares
+}
+function squaresForFillers(fillers) {
+	let squares = ''
+	let i = 0;
+	while (i <  Number(fillers)) {
+	  squares += '<div class="cell-filler dip"></div> '
+	  i++
+	}
+	return squares
+}
+function squaresForSpecial(special) {
+	let squares = ''
+	let i = 0;
+	while (i <  Number(special)) {
+	  squares += '<div class="cell-special dip"></div> '
+	  i++
+	}
+	return squares
+}
 fetch('./data/anime-top-100.csv')
 	.then(resp => resp.text())
 	.then(cartoonsCSV => {
@@ -7,19 +34,16 @@ fetch('./data/anime-top-100.csv')
 		const table = document.getElementById('episodes-table')
 
 		cartoons.forEach(item => {
-			let squares = ''
-			let i = 0;
-			while (i <  Number(item.episodes)) {
-			  squares += '<div class="cell dip"></div> '
-			  i++
-			}
-
 
 			const rowHtml = `<tr>
 	            <td>${item.rating}</td>
 	            <td>${item.title}</td>
 	            <td>${item.release}</td>
-	            <td>${squares}</td>
+	            <td>
+	            	${squaresForEpisodes(item.episodes)}
+	            	${squaresForFillers(item.fillers)}
+	            	${squaresForSpecial(item.special)}
+	            </td>
         	</tr>`;
 			table.insertAdjacentHTML('beforeend', rowHtml);
 //			document.write(item.title + '<br>')
